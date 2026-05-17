9e8f294861954ffcf2fcc16d61eab71630f2aee5
Chihayafuru1003 <htc826816@gmail.com>
2026-05-17 23:51:05 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,24 +6,28 @@ from selenium import webdriver
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-chrome_path_win = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-chrome_path_win_x86 = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
-if os.path.exists(chrome_path_win) or os.path.exists(chrome_path_win_x86):
-    from selenium.webdriver.chrome.service import Service
-    option = webdriver.ChromeOptions()
-    option.add_argument('--headless')
-    service = Service()
-    driver = webdriver.Chrome(service=service, options=option)
-    print("偵測到 Chrome，已成功啟動 Chrome 爬蟲核心")
+if os.environ.get('GITHUB_ACTIONS') == 'true':
+    print("偵測到 GitHub 雲端環境，啟用免瀏覽器 API 模式！")
+    driver = None  # 在雲端不啟動瀏覽器，避免環境報錯
 else:
-    from selenium.webdriver.edge.service import Service
-    option = webdriver.EdgeOptions()
-    option.add_argument('--headless')
-    service = Service()
-    driver = webdriver.Edge(service=service, options=option)
-    print("未偵測到 Chrome，已成功啟用 Edge 爬蟲核心")
+    chrome_path_win = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    chrome_path_win_x86 = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
+    if os.path.exists(chrome_path_win) or os.path.exists(chrome_path_win_x86):
+        from selenium.webdriver.chrome.service import Service
+        option = webdriver.ChromeOptions()
+        option.add_argument('--headless')
+        service = Service()
+        driver = webdriver.Chrome(service=service, options=option)
+        print("偵測到 Chrome，已成功啟動 Chrome 爬蟲核心")
+    else:
+        from selenium.webdriver.edge.service import Service
+        option = webdriver.EdgeOptions()
+        option.add_argument('--headless')
+        service = Service()
+        driver = webdriver.Edge(service=service, options=option)
+        print("未偵測到 Chrome，已成功啟用 Edge 爬蟲核心")
 
 def get_etf_kline_data(stock_no="0050"):
     current_date = pd.Timestamp.now()
@@ -56,10 +60,8 @@ def get_etf_kline_data(stock_no="0050"):
         except Exception as e:
             print(f"抓取 {date_str} 發生錯誤: {e}")
             
-
     columns = ['日期', '成交股數', '成交金額', '開盤價', '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數', '最後未命名欄位']
     df = pd.DataFrame(all_data, columns=columns)
-    
     df = df.drop(columns=['最後未命名欄位'])
     
     for col in ['成交股數', '開盤價', '最高價', '最低價', '收盤價']:
@@ -70,7 +72,7 @@ def get_etf_kline_data(stock_no="0050"):
 try:
     etf_df = get_etf_kline_data("0050")
     
-    etf_df.to_excel("0050_一年歷史資料.xlsx", index=False)
+    etf_df.to_excel("0050_一年歷史K線資料.xlsx", index=False)
     
     json_result = etf_df.to_json(orient="records", force_ascii=False)
     with open("etf_kline.json", "w", encoding="utf-8") as f:
@@ -79,4 +81,5 @@ try:
     print("製表完成！")
 
 finally:
-    driver.quit()
+    if driver is not None:
+        driver.quit()
